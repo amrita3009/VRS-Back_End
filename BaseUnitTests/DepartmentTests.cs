12350b1baf88ff32c09f44310ae3bb073f43d22b
harishkumar.n.sp19 <harishkumar.n.sp19@IFIYCITSKLT546>
2019-09-04 15:02:00 +0530

    Base Project

---
+++ BaseUnitTests/DepartmentTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using VRS_Base.Controllers;
+
+namespace NUnitTest.Tests
+{
+    [TestFixture]
+    public class DepartmentTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetAllDepartments()
+        {
+            var departments = new DepartmentsController();
+            var data = departments.Get().Value;
+            IEnumerable<string> expectedData = new List<string>() { "a", "b" };
+            Assert.AreEqual(expectedData, data, "departments don't match");
+
+        }
+    }
+}
---
+++ VRS_Base/Controllers/DepartmentsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VRS_Base.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var result = new string[] { "a", "b" };
+            return new JsonResult(result.ToList());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public HttpResponse Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
